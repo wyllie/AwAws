4183aa10289748bdb9ad03c161b17a4420a92c33
Andrew Wyllie <wyllie@dilex.net>
2018-10-17 08:56:38 -0400

    getting test framework sorted out

--- AwAws/Session/session.py
+++ AwAws/Session/session.py
@@ -1,21 +1,40 @@
-import boto3
+import botocore.session
 
 from AwAws.Utils.env import Env
 
+# When setting the region, the env variable will be the lowest priority
+#    + set by caller
+#    + set by credentials file
+#    + set by env
 
-class Session(boto3.session.Session):
-    def __init__(self, **kwargs):
+
+class Session():
+    def __init__(self, region_name=None):
+        self.session = None
+        self.region_name = region_name
+        return None
+
+
+    def set_session(self):
+        self.session = botocore.session.get_session()
+        return self.session
+
+
+    def set_region(self, region_name=None):
         env = Env()
 
-        region_name = None
-        if 'region_name' in kwargs:
-            region_name = str(kwargs['region_name'])
-        elif env.get_env('AWS_REGION'):
-            region_name = env.get_env('AWS_REGION')
+        if region_name is None:
+            self.region_name = env.get_env('AWS_REGION')
+        return self.region_name
 
-        super().__init__(region_name=region_name)
+
+    def get_region(self):
+        return self.region_name
 
 
     def get_client(self, service):
-        return self.client(service)
+        if self.session is None:
+            self.set_session()
+        print('SESSION', self.session.get_config_variable('credentials_file'))
+        return self.session.create_client(service, region_name=self.get_region())
 
