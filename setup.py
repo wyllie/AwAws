21dbdf043c70e852f6b7ce66b89e0d3bf57beba4
Andrew Wyllie <wyllie@dilex.net>
2020-05-17 18:53:01 -0400

    Bump version: 0.0.2 → 0.0.3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='Aws boto wrapper',
     author='Andrew Wyllie',
     author_email='wyllie@dilex.net',
-    version='0.0.2',
+    version='0.0.3',
     packages=find_packages(exclude=["*.tests", "*.tests.*", "tests.*", "tests"]),
     license='MIT License',
     long_description=open('README.md').read()
