1373532837fba2052b33098c4209882b4fd8211a
Andrew Wyllie <wyllie@dilex.net>
2020-05-19 17:45:40 -0400

    typo in Sts role_arn

--- AwAws/Session/sts.py
+++ AwAws/Session/sts.py
@@ -2,13 +2,13 @@ import boto3
 
 
 class Sts():
-    def __init__(self, role_arn=None):
+    def __init__(self):
         self.accounts = {}
         self.master_account = None
         self.org_root = None
         self.org_unit = None
         self.org_units = {}
-        self.role = role_arn
+        self.role_arn = None
         self.sts = boto3.client('sts')
 
 
@@ -19,7 +19,7 @@ class Sts():
     def assume_role(self):
         '''assume a different (remote) role than the one we are currently running in'''
         account = self.sts.assume_role(
-            RoleArn=self.set_role_arn,
+            RoleArn=self.role_arn,
             RoleSessionName="AwAwsSession"
         )
         return account['Credentials']
