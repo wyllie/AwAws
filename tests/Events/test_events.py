81bf29c0af8a651803aa547a173d24968cbf4ea8
Andrew Wyllie <wyllie@dilex.net>
2020-05-20 20:57:37 -0400

    adding test for Events module

--- tests/Events/test_events.py
+++ tests/Events/test_events.py
@@ -17,3 +17,20 @@ def test_create_event():
     chk = event.create_event('test_account')
     assert chk['Resources'][0] == "arn:aws:events:us-least-5:test_account:event-bus/test-bus"
 
+
+def test_set_event_bus_default():
+    events = Events()
+    events.set_event_bus()
+    assert events.event_bus == 'event-bus/default'
+
+
+def test_set_event_bus_set():
+    events = Events()
+    events.set_event_bus(bus_name='deploy')
+    assert events.event_bus == 'event-bus/deploy'
+
+
+def test_add_resource():
+    events = Events()
+    events.add_resource('arn.aws.etc.etc')
+    assert events.resources[0] == 'arn.aws.etc.etc'
