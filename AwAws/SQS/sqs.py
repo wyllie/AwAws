eedad51afd7e8d595c4783ad6aa479860cf7672c
Andrew Wyllie <wyllie@dilex.net>
2020-07-24 12:21:30 -0400

    Added test for sqs module and added a function to handle sending messages to a fifo queue

--- AwAws/SQS/sqs.py
+++ AwAws/SQS/sqs.py
@@ -6,8 +6,10 @@ class SQS():
     def __init__(self, region_name=None, role_arn=None):
         self.sqs_url = None
         self.sqs = Session(region_name, role_arn).get_client('sqs')
+        self.messages = []
 
     def queue_url(self, qname):
+        'given the queue name, get the associated URL'
         if qname:
             try:
                 self.sqs_url = self.sqs.get_queue_url(
@@ -19,13 +21,25 @@ class SQS():
         return self.sqs_url
 
     def send_to_sqs(self, message):
+        'send a message to a queue'
         response = self.sqs.send_message(
             QueueUrl=self.sqs_url,
             MessageBody=message
         )
         return response
 
-    def read_from_sqs(self, waittime):
+
+    def send_to_sqs_fifo(self, message, dedup_id, group_id='standard'):
+        'send a message to a FIFO queue'
+        response = self.sqs.send_message(
+            QueueUrl=self.sqs_url,
+            MessageBody=message,
+            MessageDeduplicationId=dedup_id,
+            MessageGroupId=group_id
+        )
+        return response
+
+    def read_from_sqs(self, waittime=1):
         'set a waittime to enable long polling'
         max_messages = 1
 
@@ -45,6 +59,7 @@ class SQS():
         return status
 
     def remove_from_sqs(self, receipt_handle):
+        'remove a message from a queue'
         response = self.sqs.delete_message(
             QueueUrl=self.sqs_url,
             ReceiptHandle=receipt_handle
--- tests/SQS/test_sqs.py
+++ tests/SQS/test_sqs.py
@@ -1,4 +1,5 @@
 import inspect
+import pytest
 
 from botocore.stub import Stubber
 from AwAws.SQS.sqs import SQS
@@ -8,89 +9,79 @@ sqs_test_queue = 'SQSTestQueue'
 
 
 def test_init():
-    sqs = SQS()
+    sqs = SQS(region_name='us-least-4')
     assert sqs.sqs_url is None
+    assert len(sqs.messages) == 0
     inspect.isclass(SQS)
     assert isinstance(sqs, SQS)
 
 
-# def test_queue_url():
+def test_queue_url():
+    sqs = SQS(region_name='us-least-5')
+    with Stubber(sqs.sqs) as stubber:
+        stubber.add_response('get_queue_url', {'QueueUrl': 'https://hello.there/something'})
+        sqs.queue_url('test_queue_name')
+
+        assert sqs.sqs_url == 'https://hello.there/something'
 
 
-### @mock_sqs
-### def test_send_message():
-###     create_test_queue()
-###
-###     request_queue = RequestQueue(qname=SQS_TEST_QUEUE, region='us-east-1')
-###     chk = request_queue.send_to_sqs(message='this is a test')
-###     print('CHECK', chk)
-###     assert chk['ResponseMetadata']['HTTPStatusCode'] == 200
-###
-###
-### @mock_sqs
-### def test_receive_message():
-###     create_test_queue()
-###
-###     request_queue = RequestQueue(qname=SQS_TEST_QUEUE, region='us-east-1')
-###     request_queue.send_to_sqs(message='zero this is a test')
-###     request_queue.send_to_sqs(message='one this is a test')
-###     request_queue.send_to_sqs(message='two this is a test')
-###     request_queue.send_to_sqs(message='three this is a test')
-###
-###     # now set up a second RequestQueue Object and query the queue
-###     new_queue = RequestQueue(qname=SQS_TEST_QUEUE, region='us-east-1')
-###     status = new_queue.read_from_sqs(num_msg=4)
-###
-###     assert status == 4
-###     assert new_queue.messages[3].body == 'three this is a test'
-###
-###
-### @mock_sqs
-### def test_receive_message_count():
-###     create_test_queue()
-###
-###     request_queue = RequestQueue(qname=SQS_TEST_QUEUE, region='us-east-1')
-###     request_queue.send_to_sqs(message='zero this is a test')
-###     request_queue.send_to_sqs(message='one this is a test')
-###     request_queue.send_to_sqs(message='two this is a test')
-###     request_queue.send_to_sqs(message='three this is a test')
-###
-###     # now set up a second RequestQueue Object and query the queue
-###     new_queue = RequestQueue(qname=SQS_TEST_QUEUE, region='us-east-1')
-###     status = new_queue.read_from_sqs(num_msg=1)
-###
-###     assert status == 1
-###     assert new_queue.messages[0].body == 'zero this is a test'
-###     assert new_queue.messages[0].receive_count == 1
-###
-###
-### @mock_sqs
-### def test_receive_from_empty_queue():
-###     create_test_queue()
-###
-###     request_queue = RequestQueue(qname=SQS_TEST_QUEUE, waittime=2, region='us-east-1')
-###     msg = request_queue.read_from_sqs()
-###
-###     assert msg is None
-###
-###
-### @mock_sqs
-### def test_delete_message():
-###     create_test_queue()
-###
-###     # create a message and send it to the queue
-###     request_queue = RequestQueue(qname=SQS_TEST_QUEUE, region='us-east-1')
-###     request_queue.send_to_sqs(message='this is a test')
-###
-###     # read the message from the queue
-###     new_queue = RequestQueue(qname=SQS_TEST_QUEUE, region='us-east-1')
-###     msg = new_queue.read_from_sqs()
-###
-###     assert msg is not None
-###     assert new_queue.messages[0].body == 'this is a test'
-###     receipt = new_queue.messages[0].receipt_handle
-###
-###     res = new_queue.remove_from_sqs(receipt)
-###     assert res['ResponseMetadata']['HTTPStatusCode'] == 200
-###
-###
+def test_queue_url_error():
+    sqs = SQS(region_name='us-least-5')
+    with Stubber(sqs.sqs) as stubber:
+        stubber.add_response('get_queue_url', {'QueueUrl': 'nada'}, {'QueueName': 'hello'})
+
+        with pytest.raises(Exception, match=r'Could not find SQS Queue'):
+            sqs.queue_url('test_queue_name')
+
+
+def test_send_to_sqs():
+    sqs = SQS(region_name='us-least-6')
+    with Stubber(sqs.sqs) as stubber:
+        stubber.add_response('get_queue_url', {'QueueUrl': 'https://hello.there/something'})
+        stubber.add_response('send_message', {'MessageId': '1'})
+
+        sqs.queue_url('test_queue_name')
+        check = sqs.send_to_sqs('some stuff')
+        assert check['MessageId'] == '1'
+
+
+def test_send_to_sqs_fifo():
+    sqs = SQS(region_name='us-least-6')
+    with Stubber(sqs.sqs) as stubber:
+        stubber.add_response('get_queue_url', {'QueueUrl': 'https://hello.there/something'})
+        stubber.add_response('send_message', {'MessageId': '1'})
+
+        sqs.queue_url('test_queue_name')
+        check = sqs.send_to_sqs_fifo('some stuff', '12345', group_id='hello')
+        assert check['MessageId'] == '1'
+
+
+def test_read_from_sqs():
+    msg_response = {
+        'Body': 'This is the message we sent',
+        'ReceiptHandle': 'your receipt, thank you',
+        'MessageId': 'MessageId is not a number',
+        'Attributes': {'ApproximateReceiveCount': '1'}
+    }
+    sqs = SQS(region_name='us-least-6')
+    with Stubber(sqs.sqs) as stubber:
+        stubber.add_response('get_queue_url', {'QueueUrl': 'https://hello.there/something'})
+        stubber.add_response('receive_message', {'Messages': [msg_response]})
+
+        sqs.queue_url('test_queue_name')
+        sqs.read_from_sqs()
+        assert len(sqs.messages) == 1
+        assert sqs.messages[0].body == 'This is the message we sent'
+        assert sqs.messages[0].receipt_handle == 'your receipt, thank you'
+        assert sqs.messages[0].message_id == 'MessageId is not a number'
+        assert sqs.messages[0].receive_count == 1
+
+
+def test_remove_from_sqs():
+    sqs = SQS(region_name='us-least-6')
+    with Stubber(sqs.sqs) as stubber:
+        stubber.add_response('get_queue_url', {'QueueUrl': 'https://hello.there/something'})
+        stubber.add_response('delete_message', {})
+
+        sqs.queue_url('test_queue_name')
+        sqs.remove_from_sqs('some_handle')
