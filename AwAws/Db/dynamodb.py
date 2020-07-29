c24ec403568f1ed405dbf2b5a931a1ea24488c15
Andrew Wyllie <wyllie@dilex.net>
2020-07-29 13:51:16 -0400

    Add DynamoDB module and tests

--- AwAws/Db/dynamodb.py
+++ AwAws/Db/dynamodb.py
@@ -1,23 +1,42 @@
+from boto3.dynamodb.conditions import Key
 from AwAws.Session.resource import Resource
 
 
-class DynamoDb():
+class DynamoDb:
     def __init__(self, table_name, region_name=None):
         self.table_name = table_name
         self.dynamo = Resource(region_name).get_resource('dynamodb')
-        self.table = self._get_table_object(table_name)
+        self.table = self._get_table_object()
+
+    def delete_item(self, primary_key_id):
+        'delete an item from a dynamo table by primary key id'
+        return self.table.delete_item(Key={'S': primary_key_id})
+
+    def get_item(self, primary_key_id):
+        'get an item from a dynamo table by primary key id'
+        return self.table.get_item(Key={'S': primary_key_id})
 
     def put_item(self, item):
         'put item in dynamoDb table'
-        table = self.table
-        res = table.put_item(Item=item)
-        return res
+        return self.table.put_item(Item=item)
 
-    def _get_table_object(self, table_name):
+    def query_by_partition_key(self, partition_key, partition_key_value=None):
+        'get a list of items by primary_key and sort_key'
+        query = Key(partition_key).eq(partition_key_value)
+
+        return self.table.query(KeyConditionExpression=query)
+
+    def query_by_partition_and_sort_key(self, partition_key, partition_key_value,
+                                        sort_key, sort_key_value):
+        'get a list of items by primary_key and sort_key'
+        return self.table.query(
+            KeyConditionExpression=Key(partition_key).eq(partition_key_value) &
+            Key(sort_key).begins_with(sort_key_value)
+        )
+
+
+    def _get_table_object(self):
         'gets a dynamodb.Table resource'
-        if table_name is not None:
-            self.table_name = table_name
-
         try:
             table = self.dynamo.Table(self.table_name)
         except Exception as e:
--- tests/Db/test_dynamodb.py
+++ tests/Db/test_dynamodb.py
@@ -9,6 +9,8 @@ def test_init():
     db = DynamoDb('test_table')
     assert inspect.isclass(DynamoDb)
     assert isinstance(db, DynamoDb)
+
+    # checking to make sure we got the correct boto classes
     assert str(type(db.dynamo)) == "<class 'boto3.resources.factory.dynamodb.ServiceResource'>"
     assert str(type(db.table)) == "<class 'boto3.resources.factory.dynamodb.Table'>"
 
@@ -19,12 +21,69 @@ def test_init_fails():
         DynamoDb()
 
 
-def test_put_item():
-    db = DynamoDb('test_table')
-    stubber = Stubber(db.table.meta.client)
-    stubber.add_response('put_item', {'ConsumedCapacity': {'TableName': 'blargh'}})
-    stubber.activate()
+def test_delete_item():
+    db = DynamoDb('blargh')
+    with Stubber(db.table.meta.client) as stubber:
+        stubber.add_response('delete_item', {
+            'ConsumedCapacity': {'TableName': 'blargh'}
+        })
 
-    item = {'id': 1, 'this': 'text'}
-    chk = db.put_item(item)
-    assert chk['ConsumedCapacity']['TableName'] == 'blargh'
+        chk = db.delete_item('some_key')
+        assert chk['ConsumedCapacity']['TableName'] == 'blargh'
+
+
+def test_get_item():
+    db = DynamoDb('blargh')
+    with Stubber(db.table.meta.client) as stubber:
+        item = {'id': {'S': 'some_key_id'}, 'this': {'S': 'some test text'}}
+        stubber.add_response('get_item', {
+            'Item': item,
+            'ConsumedCapacity': {'TableName': 'blargh'}
+        })
+
+        chk = db.get_item('some_key')
+        assert chk['ConsumedCapacity']['TableName'] == 'blargh'
+        assert chk['Item']['id'] == 'some_key_id'
+        assert chk['Item']['this'] == 'some test text'
+
+
+def test_put_item():
+    db = DynamoDb('blargh')
+    with Stubber(db.table.meta.client) as stubber:
+        stubber.add_response('put_item', {'ConsumedCapacity': {'TableName': 'blargh'}})
+
+        item = {'id': 1, 'this': 'text'}
+        chk = db.put_item(item)
+        assert chk['ConsumedCapacity']['TableName'] == 'blargh'
+
+
+def test_query_by_partition_key():
+    db = DynamoDb('blargh')
+    with Stubber(db.table.meta.client) as stubber:
+        items = [
+            {'id': {'S': 'some_key_id'}, 'this': {'S': 'some test text'}},
+            {'id': {'S': 'some_other_key_id'}, 'this': {'S': 'other test text'}}
+        ]
+        stubber.add_response('query', {'Items': items})
+
+        chk = db.query_by_partition_key('id')
+        assert chk['Items'][0]['id'] == 'some_key_id'
+        assert chk['Items'][0]['this'] == 'some test text'
+        assert chk['Items'][1]['id'] == 'some_other_key_id'
+        assert chk['Items'][1]['this'] == 'other test text'
+
+
+def test_query_by_partition_and_sort_key():
+    db = DynamoDb('blargh')
+    with Stubber(db.table.meta.client) as stubber:
+        items = [
+            {'id': {'S': 'some_key_id'}, 'this': {'S': 'some test text'}},
+            {'id': {'S': 'some_other_key_id'}, 'this': {'S': 'other test text'}}
+        ]
+        stubber.add_response('query', {'Items': items})
+
+        chk = db.query_by_partition_and_sort_key('id', None, 'this', None)
+        assert chk['Items'][0]['id'] == 'some_key_id'
+        assert chk['Items'][0]['this'] == 'some test text'
+        assert chk['Items'][1]['id'] == 'some_other_key_id'
+        assert chk['Items'][1]['this'] == 'other test text'
