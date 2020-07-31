c4c12ab63715cf1ad2ed0708d93da57e8b534b5a
Andrew Wyllie <wyllie@dilex.net>
2020-07-31 11:28:27 -0400

    Complete rewrite of the SSM Parameter Store core to make it a lot easier to use

--- AwAws/SharedResources/parameters.py
+++ AwAws/SharedResources/parameters.py
@@ -2,54 +2,63 @@ from AwAws.Session.session import Session
 
 
 class Parameters:
-    def __init__(self, env=None, service=None, name=None):
-
-        self.env = env
+    'parameters are store as <service>.<param> = value'
+    def __init__(self, service, region_name=None, role_arn=None):
         self.service = service
-        self.name = name
-        self.value = None
-        self.ssm = None
-        self.fully_qualified_name = self.fully_qualified_parameter_name()
+        self.ssm = Session(region_name, role_arn).get_client('ssm')
 
-
-    def get_client(self, client=None):
-        self.ssm = Session().get_client('ssm')
-        return self.ssm
-
-
-    def set_value(self, value):
-        self.value = value
-
-
-    def get(self):
+    def get_param(self, name=None):
+        'returns a specific parameter from service'
+        fqn = self.fully_qualified_parameter_name(name)
         try:
             response = self.ssm.get_parameter(
-                Name=self.fully_qualified_name
+                Name=fqn,
+                WithDecryption=True
             )
-            self.value = response['Parameter']['Value']
         except Exception as e:
-            raise RuntimeError('Could not find: ' + self.fully_qualified_name +
-                               ' ' + str(e))
-        return self.value
+            raise RuntimeError('Could not find: ', fqn, e)
+        return response['Parameter']
 
+    def get_param_value(self, name=None):
+        return self.get_param(name)['Value']
 
-    def put(self, value, overwrite=False):
-        self.set_value(value)
+    def get_all(self):
+        'returns all of the params related to this service'
+        service_root = self.fully_qualified_parameter_name('')
+        try:
+            response = self.ssm.get_parameters_by_path(
+                Path=service_root,
+                WithDecryption=True
+            )
+        except Exception as e:
+            raise RuntimeError('Could not find: ', service_root, e)
+
+        # return a hash so they are easy to lookup
+        ret_params = {}
+        for param in response['Parameters']:
+            name = param['Name'].split('/')[2]
+            ret_params[name] = param
+        return ret_params
+
+    def put_param(self, name, value, secure=None):
+        'stores a parameter in the SSM parameter store'
+        fqn = self.fully_qualified_parameter_name(name)
+        param_type = 'String'
+        if secure == 'True':
+            param_type = 'SecureString'
+        elif isinstance(value, list):
+            param_type = 'StringList'
+
         try:
             response = self.ssm.put_parameter(
-                Name=self.fully_qualified_name,
-                Value=self.value,
-                Type='String',
-                Overwrite=overwrite
+                Name=fqn,
+                Value=value,
+                Type=param_type,
+                Overwrite=True
             )
         except Exception as e:
-            raise RuntimeError('Could not set: ' + self.fully_qualified_name +
-                               ' ' + str(e))
+            raise RuntimeError('Could not set: ', fqn, e)
         return response['Version']
 
-
-    def fully_qualified_parameter_name(self):
-        return '.'.join([self.env, self.service, self.name])
-
-
-
+    def fully_qualified_parameter_name(self, name):
+        return '/'.join(['', self.service, name])
--- tests/SharedResources/test_parameters.py
+++ tests/SharedResources/test_parameters.py
@@ -1,90 +1,105 @@
+import inspect
 import pytest
 
 from botocore.stub import Stubber
 from AwAws.SharedResources.parameters import Parameters
 
 
+# set up some simple responses from SSM
+@pytest.fixture
+def ssm_get_response():
+    response = {
+        'Parameter': {
+            'Type': 'String',
+            'Name': '/alpha/hostname',
+            'Value': 'some.hostname.com'
+        }
+    }
+    return response
+
+
+@pytest.fixture
+def ssm_get_all_response():
+    response = {
+        'Parameters': [
+            {
+                'Type': 'String',
+                'Name': '/alpha/hostname',
+                'Value': 'some.hostname.com'
+            },
+            {
+                'Type': 'SecureString',
+                'Name': '/alpha/password',
+                'Value': 'p@ssw0rd123'
+            },
+        ]
+    }
+    return response
+
+
+@pytest.fixture
+def ssm_put_response():
+    response = {
+        'Version': 1234
+    }
+    return response
+
+
 def test_init():
-    params = Parameters(env='test-env', service='test-service', name='test-name')
-    assert params.env == 'test-env'
-    assert params.service == 'test-service'
-    assert params.name == 'test-name'
-    assert params.value is None
-    assert params.fully_qualified_name == 'test-env.test-service.test-name'
+    params = Parameters(service='test_service')
+    inspect.isclass(Parameters)
+    assert str(type(params.ssm)) == "<class 'botocore.client.SSM'>"
+    assert params.service == 'test_service'
 
 
-def test_get_client():
-    params = Parameters(env='test-env', service='test-service', name='test-name')
-    client = params.get_client()
-    assert str(type(client)) == "<class 'botocore.client.SSM'>"
-
-
-def test_set_value():
-    params = Parameters(env='test-env', service='test-service', name='test-name')
-    assert params.value is None
-    params.set_value('test-value')
-    assert params.value == 'test-value'
-
-
-def test_get(ssm_get_response):
-    env = 'test'
-    service = 'mongo'
-    name = 'hostname'
-
-    params = Parameters(env, service, name)
-    ssm = params.get_client()
-    with Stubber(ssm) as stubber:
+def test_get_param(ssm_get_response):
+    params = Parameters(service='alpha')
+    with Stubber(params.ssm) as stubber:
         stubber.add_response('get_parameter', ssm_get_response)
-        assert params.get() == 'xtest.mongo.com'
+        assert params.get_param('hostname')['Value'] == 'some.hostname.com'
+
+
+def test_get_param_value(ssm_get_response):
+    params = Parameters(service='alpha')
+    with Stubber(params.ssm) as stubber:
+        stubber.add_response('get_parameter', ssm_get_response)
+        assert params.get_param_value('hostname') == 'some.hostname.com'
 
 
 def test_get_fail():
-    env = 'test'
-    service = 'mongo-fail'
-    name = 'hostname'
-
-    params = Parameters(env, service, name)
-    ssm = params.get_client()
-    with Stubber(ssm) as stubber:
+    params = Parameters(service='omega')
+    with Stubber(params.ssm) as stubber:
         stubber.add_client_error('get_parameter')
         with pytest.raises(RuntimeError):
-            params.get()
+            params.get_param('hostname')
 
 
-def test_put(ssm_put_response):
-    env = 'test'
-    service = 'mongo'
-    name = 'hostname'
+def test_get_all(ssm_get_all_response):
+    params = Parameters(service='test_service')
+    with Stubber(params.ssm) as stubber:
+        stubber.add_response('get_parameters_by_path', ssm_get_all_response)
+        my_params = params.get_all()
+        assert my_params['hostname']['Value'] == 'some.hostname.com'
+        assert my_params['password']['Value'] == 'p@ssw0rd123'
 
-    params = Parameters(env, service, name)
-    ssm = params.get_client()
-    with Stubber(ssm) as stubber:
+
+def test_put_param(ssm_put_response):
+    params = Parameters(service='alpha')
+    with Stubber(params.ssm) as stubber:
         stubber.add_response('put_parameter', ssm_put_response)
         stubber.add_response('put_parameter', ssm_put_response)
-        assert params.put('test.mongo.name') == 1234
-        assert params.put('test.mongo.name', True) == 1234
+        assert params.put_param('param1', 'test1.mongo.name') == 1234
+        assert params.put_param('param2', 'test2.mongo.name', secure=True) == 1234
 
 
 def test_put_fail():
-    env = 'test'
-    service = 'mongo'
-    name = 'hostname'
-
-    params = Parameters(env, service, name)
-    ssm = params.get_client()
-    with Stubber(ssm) as stubber:
+    params = Parameters(service='omega')
+    with Stubber(params.ssm) as stubber:
         stubber.add_client_error('put_parameter')
         with pytest.raises(RuntimeError):
-            params.put(None)
+            params.put_param('a', 'b')
 
 
 def test_fully_qualified_parameter_name():
-    env = 'test'
-    service = 'database'
-    name = 'hostname'
-
-    params = Parameters(env, service, name)
-    assert params.fully_qualified_parameter_name() == 'test.database.hostname'
-
-    with pytest.raises(TypeError):
-        Parameters(env, None, name)
+    params = Parameters(service='beta')
+    assert params.fully_qualified_parameter_name('hello') == '/beta/hello'
