4931ab1149a5c333a68bdde52f43818212edb327
Andrew Wyllie <wyllie@dilex.net>
2020-08-16 20:04:24 -0400

    Bump version: 0.0.16 → 0.0.17

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Dilex Networks'
 author = 'Dilex Networks'
 
 # The full version, including alpha/beta/rc tags
-release = 'v0.0.16'
+release = 'vrelease==0.0.17'
 
 
 # -- General configuration ---------------------------------------------------
