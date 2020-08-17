d11af3c585af3ec88fa131b76bc177405c26bf8d
Andrew Wyllie <wyllie@dilex.net>
2020-08-16 20:06:49 -0400

    fixing version numbers

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Dilex Networks'
 author = 'Dilex Networks'
 
 # The full version, including alpha/beta/rc tags
-release = 'vrelease==0.0.17'
+release = 'v0.0.17'
 
 
 # -- General configuration ---------------------------------------------------
