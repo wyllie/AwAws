b988839ab48e04984ff221a16d5f662380860197
Andrew Wyllie <wyllie@dilex.net>
2020-08-16 20:04:05 -0400

    adding versions to docs

--- docs/conf.py
+++ docs/conf.py
@@ -23,7 +23,7 @@ copyright = '2020, Dilex Networks'
 author = 'Dilex Networks'
 
 # The full version, including alpha/beta/rc tags
-release = 'v1.0.0'
+release = 'v0.0.16'
 
 
 # -- General configuration ---------------------------------------------------
